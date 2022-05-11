36496064892f3e290f49cb7c82676fc4777a6ee7
romangrbr <romangrbr@gmail.com>
2022-05-11 16:58:04 +0300

    bot2

--- lottery_bot.py
+++ lottery_bot.py
@@ -6,20 +6,15 @@ API_HASH = "9c2dd8b31f65db8ff63abe5b73d879de"
 LOTTERY_SKANNER_GROUP_ID = -1001141596012
 LOTTERY_SKANNER_GROUP = 'LotterySkanner'
 METROPOLIS_ID = -1001539175416
-METROPOLIS = 'metropolisru'
+metropolis = 'metropolisru'
 client = TelegramClient('anon', API_ID, API_HASH)
 brief = -1001100073584
 
-# @client.on(events.NewMessage)
-# async def my_event_handler(event):
-#     if 'hello' in event.raw_text:
-#         await client.send_message(LOTTERY_SKANNER_GROUP_ID, 'Hi!')
 
-@client.on(events.NewMessage(chats=('brief', 'LotterySkanner', 'vestiru24')))
+@client.on(events.NewMessage(chats=('brief', 'vestiru24')))
 async def my_event_handler(event):
     if 'розыгрыш' in event.raw_text.lower():
-        # await client.send_message(LOTTERY_SKANNER_GROUP_ID, event.raw_text)
-        await client.forward_messages(LOTTERY_SKANNER_GROUP_ID, messages=event.raw_text)
+        await client.forward_messages(LOTTERY_SKANNER_GROUP_ID, event.message)
 
 client.start()
 client.run_until_disconnected()
