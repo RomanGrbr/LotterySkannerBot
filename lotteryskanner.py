910f64b03b4ef49e682af5a692628c73c209b7b4
Роман <romangrbr@gmail.com>
2022-05-15 20:55:47 +0300

    add sqlite

--- lotteryskanner.py
+++ lotteryskanner.py
@@ -1,26 +1,72 @@
 import re
-from telethon.sync import TelegramClient, events, utils
-from config import (API_HASH, API_ID, SESSION_STRING, TARGET_CHANNEL)
+
+from telethon.sync import TelegramClient, events
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, PeerChannel
+
+from config import API_HASH, API_ID, SESSION_STRING, TARGET_CHANNEL
+from dbhelper import DBHelper
 
 client = TelegramClient(SESSION_STRING, API_ID, API_HASH)
+client.connect()
+db = DBHelper()
+db.setup()
 
-lotteryskanner = -1001569559960
-brief = -1001099350027
-vesti24 = -1001001872252
-nezigar = -1001096463945
+async def channel_update():
+    id_list = db.get_items()
+    for i in id_list:
+        db.delete_item(i)
+    
+    get_dialogs = GetDialogsRequest(
+        offset_date=None,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=30,
+        hash=0
+    )
 
-@client.on(events.NewMessage(chats=[lotteryskanner, brief, vesti24, nezigar]))
+    dialogs = client(get_dialogs)
+    for d in dialogs.dialogs:
+        peer = d.peer
+        if isinstance(peer, PeerChannel):
+            id = peer.channel_id
+            db.add_item(id)
+
+
+channel_update()
+print(db.get_items())
+
+# my_channel = set()
+# get_dialogs = GetDialogsRequest(
+#     offset_date=None,
+#     offset_id=0,
+#     offset_peer=InputPeerEmpty(),
+#     limit=30,
+#     hash=0
+# )
+
+# dialogs = client(get_dialogs)
+# for d in dialogs.dialogs:
+#     peer = d.peer
+#     if isinstance(peer, PeerChannel):
+#         id = peer.channel_id
+#         my_channel.add(id)
+# print(my_channel)
+
+@client.on(events.NewMessage(chats=db.get_items()))
 async def handler_new_message(event):
     try:
-        if re.findall(r'розыгры', event.raw_text.lower()):
+        if re.findall(r'розыгры', event.raw_text.lower()) or (
+            re.findall(r'конкурс', event.raw_text.lower())):
             await client.forward_messages(int(TARGET_CHANNEL), event.message)
-        # elif event:
-        #     chat_from = event.chat if event.chat else (await event.get_chat())
-        #     chat_title = utils.get_display_name(chat_from)
-        #     chat = f'Название **{chat_title}**, id: **{event.chat_id}**'
-        #     await client.send_message(int(TARGET_CHANNEL), chat)
     except Exception as e:
         print(e)
+
+@client.on(events.NewMessage(chats=int(TARGET_CHANNEL)))
+async def handler(event):
+    if event.raw_text.lower() == '/channel':
+        channel_update()
+        await client.send_message(int(TARGET_CHANNEL), 'channel update')
 
 
 if __name__ == '__main__':
